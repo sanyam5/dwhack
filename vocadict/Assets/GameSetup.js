3e7178884124e3177da1cb8228f01e338042047c
Akshay Gupta <akshaysngupta@gmail.com>
2014-01-07 22:43:38 +0530

    scripts added

---
+++ vocadict/Assets/GameSetup.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var letter1:Transform;
+var letter2:Transform;
+var letter3:Transform;
+
+function Start () {
+	letter1.position = Vector3 (-100f,-100f,-100f);
+	letter2.position = Vector3 (-100f,-100f,-100f);
+	letter3.position = Vector3 (-100f,-100f,-100f);
+}
+
+function Update () {
+	
+}
